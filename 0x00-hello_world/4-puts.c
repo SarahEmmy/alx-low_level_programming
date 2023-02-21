0c603a38bb387f5011e6df0d26067644e3d55861
SarahEmmy <bawahsarah@gmail.com>
2023-02-21 01:22:52 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints"Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle,
- * Return: Always 0,
+ * Return: Always 0.
  */
 int main(void)
 {
