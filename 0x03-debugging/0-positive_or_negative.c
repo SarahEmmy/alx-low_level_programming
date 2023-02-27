41642497a6365d2db1d4f514c9d40e09638b71e7
SarahEmmy <bawahsarah@gmail.com>
2023-02-27 08:38:47 -0800

    0-main.c

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Start point for program
+ * Return: Always 0 is success
+ */
+int main(void)
+{
+	int n = 0;
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n >= 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	printf("%d is negative\n", n);
+	return (0);
+}
