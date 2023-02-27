e530286d910ec71aa6638e1a6a111b951bc55200
SarahEmmy <bawahsarah@gmail.com>
2023-02-27 07:40:02 -0800

    A function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table,
- * starting with 0
- * @n: number of the times table
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the size of the times table to print
+ *
+ * Return: void
  */
-
-void print_times_table(int n){
-int i, j, k;
-if (n >= 0 && n <= 15){
-for (i = 0; i <= n; i++){
-for (j = 0; j <= n; j++){
-k = j * i;
-if (j == 0){
-_putchar(k + '0');
-} 
-else if (k < 10 && j != 0){
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-} else if (k >= 10 && k < 100){
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-} else if (k >= 100)
+void print_times_table(int n)
 {
-_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
+    int i, j;
+
+    if (n < 0 || n > 15)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            int res = i * j;
+
+            if (j == 0)
+                _putchar('0');
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+
+            if (res < 10)
+            {
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(res + '0');
+            }
+            else if (res < 100)
+            {
+                _putchar(' ');
+                _putchar(res / 10 + '0');
+                _putchar(res % 10 + '0');
+            }
+            else
+            {
+                _putchar(res / 100 + '0');
+                _putchar(res / 10 % 10 + '0');
+                _putchar(res % 10 + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
-}
-}
-}
-}                  
