cd1786caf7ce0c0751627024ad12d1d4e6be62cd
SarahEmmy <bawahsarah@gmail.com>
2023-06-21 01:15:24 -0700

    updates

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - performs addition of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: the result of the addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - performs subtraction of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: the result of the subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - performs multiplication of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: the result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - performs division of two numbers.
+ * @a: dividend.
+ * @b: divisor.
+ * Return: the result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - performs modulo operation on two numbers.
+ * @a: dividend.
+ * @b: divisor.
+ * Return: the remainder of the division.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
