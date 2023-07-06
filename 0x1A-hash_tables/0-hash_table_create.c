fba3b52297a8d6ab7d0be192f624f1e57e8ac3d0
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-07 00:18:39 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,29 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_create - creates a hash tables
- * @size: the array (hash table) size
- * Return: a  pointer to the newly created hash table
+ * hash_table_create - creates a hash table
+ * @size: the size of the array (hash table)
+ *
+ * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ptr_to_array;
+    hash_table_t *hash_table;
 
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+        return (NULL);
 
-	ptr_to_array = calloc(1, sizeof(hash_table_t));
-	if (ptr_to_array == NULL)
-		return (NULL);
-	ptr_to_array->size = size;
-	/* The  calloc()  function allocates memory for an array */
-	/* void *calloc(size_t nmemb, size_t size) */
-	ptr_to_array->array = calloc(size, sizeof(hash_table_t **));
-	if (ptr_to_array->array == NULL)
-	{
-		free(ptr_to_array);
-		return (NULL);
-	}
-	return (ptr_to_array);
+    hash_table = malloc(sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return (NULL);
+
+    hash_table->size = size;
+    hash_table->array = calloc(size, sizeof(hash_node_t *));
+    if (hash_table->array == NULL)
+    {
+        free(hash_table);
+        return (NULL);
+    }
+
+    return (hash_table);
 }
