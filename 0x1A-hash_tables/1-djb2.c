f8e19ce89efa648043527b9f824c5dce890f19c1
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-07 00:23:59 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
+
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- * Return: hash value
+ * hash_djb2 - Implementation of the djb2 algorithm.
+ * @str: The string used to generate the hash value.
+ *
+ * Return: The hash value.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
+    unsigned long int hash = 5381;
+    int c;
 
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return (hash);
 }
