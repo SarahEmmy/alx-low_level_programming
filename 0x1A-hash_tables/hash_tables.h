9792e600a0010ca2e11ee696511df6fdf8011652
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-07 00:21:48 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,8 +5,6 @@
 #include <string.h> /* strdup, strcmp */
 #include <stdio.h> /* exit status */
 
-/* given data structures */
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -17,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -32,20 +30,66 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
+/* Function Prototypes */
 
-/* functions protoypes */
-
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size);
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The hash value.
+ */
 unsigned long int hash_djb2(const unsigned char *str);
+
+/**
+ * key_index - Retrieves the index of a key.
+ * @key: The key.
+ * @size: The size of the array.
+ *
+ * Return: The index at which the key/value pair should be stored in the array.
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table.
+ * @key: The key. Cannot be an empty string.
+ * @value: The value associated with the key. Must be duplicated.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table.
+ * @key: The key.
+ *
+ * Return: The value associated with the element, or NULL if key couldn't be found.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ */
 void hash_table_print(const hash_table_t *ht);
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table.
+ */
 void hash_table_delete(hash_table_t *ht);
 
-
-#endif
+#endif /* _HASH_TABLES_H */
