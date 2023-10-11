3dca012a031008a5f263f04592169335b382f7a9
SarahB <122871970+SarahEmmy@users.noreply.github.com>
2023-10-11 14:45:53 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,14 +8,13 @@
 
 /* STRUCTS */
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
+ * struct listint_s - Singly Linked List Node
+ * @n: Integer value stored in the node
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: Defines a structure for a singly linked list node
+ * used in Holberton project.
  */
 typedef struct listint_s
 {
@@ -25,15 +24,14 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
+ * struct skiplist_s - Singly Linked List with an Express Lane
+ * @n: Integer value stored in the node
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
+ * Description: Defines a structure for a singly linked list node with an
+ *              express lane used in the Holberton project.
  */
 typedef struct skiplist_s
 {
